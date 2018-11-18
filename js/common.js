576b62ca6b98bfbbd2ddf03a0a2388c8592853dc
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-11-19 00:40:31 +0800

    1119

---
+++ js/common.js
@@ -0,0 +1,26 @@
+﻿jQuery(document).ready(function(e) {
+	/* 平板旋轉設定 */
+	jQuery(window).on('orientationchange', function(e) {
+			jQuery(window).trigger('resize');
+	});	
+	jQuery(window).on('orientationchange', function(e) {
+    window.scroll(0, 0);
+  });
+	
+	//置頂
+	var $tBEle = $('#goTop').on('click', function(e) {
+		e.preventDefault();
+		jQuery('html, body').animate({scrollTop: 0}, {queue: false, duration: 800});
+	})
+	// Menu    
+	jQuery('.menubox li.menuLi').hover(function(e) {
+		var $cEle = jQuery(this);
+		$cEle.find('a.mlink').addClass('');    
+		$cEle.children('div.navSub').stop(false, true).fadeIn(300);
+	}, function(e) {
+		var $cEle = jQuery(this);
+		$cEle.find('a.mlink').removeClass('');  
+		$cEle.children('div.navSub').stop(false, true).fadeOut(300);  
+	});
+	
+});
